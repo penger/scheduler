3c09c356b87e898ca4584e3fbe5ba3699362d274
penger <esc_penger@163.com>
2018-07-23 14:45:19 +0800

    sum commit

--- src/test/java/com/cqw/lucence/CreateIndex.java
+++ src/test/java/com/cqw/lucence/CreateIndex.java
@@ -8,16 +8,14 @@ import java.io.InputStreamReader;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.TextField;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
-
-import javafx.scene.shape.Line;
 
 public class CreateIndex {
     
